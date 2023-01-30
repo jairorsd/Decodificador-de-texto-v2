7489e6fd3bf754acae91856316b1e319a5c34fc8
Jairo <paypal061994@gmail.com>
2023-01-30 20:30:51 -0300

    primeiro commit

---
+++ editar.js
@@ -0,0 +1,271 @@
+const indicesDosInputsRemovidosDescriptografados = [];
+const indicesDosInputsRemovidosCriptografados = [];
+
+const sectionPrincipal = document.querySelector('.principal');
+const btnEditar = document.querySelector('.btn-editar');
+const edit = document.createElement('div'); // 
+const fechaEditContainer = document.createElement('div');
+const btnFechaEdit = document.createElement('button');
+const form = document.createElement('form');
+const mainContainerInputs = document.createElement('div');
+const btnAdicionar = document.createElement('button');
+const salvarContainer = document.createElement('div');
+const btnSalvar = document.createElement('button');
+
+edit.classList.add('editar');
+fechaEditContainer.classList.add('fechar-edit-container');
+btnFechaEdit.classList.add('btn-fechar');
+btnFechaEdit.innerHTML = '✕';
+form.classList.add('form-editar');
+mainContainerInputs.classList.add('main-container-inputs');
+btnAdicionar.classList.add('adicionar');
+btnAdicionar.innerHTML = 'Adicionar';
+btnSalvar.classList.add('salvar');
+btnSalvar.innerHTML = 'Salvar';
+salvarContainer.classList.add('salvar-container');
+
+fechaEditContainer.appendChild(btnFechaEdit);
+form.appendChild(mainContainerInputs);
+salvarContainer.appendChild(btnAdicionar);
+salvarContainer.appendChild(btnSalvar);
+
+function mostraEditar() {
+
+    let posicao = 0;
+
+    for (let i = 0; i < descriptografados.length; i++) {
+
+        while (posicao < criptografados.length) {
+            criaElementosDoForm(descriptografados[i], criptografados[posicao]);
+            break;
+        }
+        posicao++
+    };
+    edit.scrollIntoView({ block: "center", behavior: "smooth" });
+};
+
+function criaElementosDoForm(descriptografados = '', criptografados = '') {
+
+    const containerInputs = document.createElement('div');
+    const inputTextoContainer = document.createElement('div');
+    const descriptografadosInputTexto = document.createElement('input');
+    const simboloDeSeta = document.createElement('span');
+    const criptografadosInputTexto = document.createElement('input');
+    const containerBotoes = document.createElement('div');
+    const btnExcluir = document.createElement('button');
+
+    containerInputs.classList.add('container-inputs');
+    inputTextoContainer.classList.add('input-texto-container');
+    descriptografadosInputTexto.classList.add('descriptografados');
+    criptografadosInputTexto.classList.add('criptografados');
+    btnExcluir.classList.add('btn-excluir');
+
+    btnExcluir.innerHTML = 'Excluir';
+    descriptografadosInputTexto.required = true;
+    criptografadosInputTexto.required = true;
+
+    containerBotoes.appendChild(btnExcluir);
+    inputTextoContainer.appendChild(descriptografadosInputTexto);
+    descriptografadosInputTexto.after(simboloDeSeta);
+    inputTextoContainer.appendChild(criptografadosInputTexto);
+    containerInputs.appendChild(inputTextoContainer);
+    containerInputs.appendChild(containerBotoes);
+    mainContainerInputs.appendChild(containerInputs);
+
+    descriptografadosInputTexto.value = descriptografados;
+    criptografadosInputTexto.value = criptografados;
+
+    edit.appendChild(fechaEditContainer);
+    edit.appendChild(form);
+    form.appendChild(salvarContainer);
+    sectionPrincipal.appendChild(edit);
+};
+
+function verificaSeAlgumCampoFoiModificado() {
+
+    const listaInputsTextoDescriptografados = form.querySelectorAll('.descriptografados');
+    const listaInputsCriptografados = form.querySelectorAll('.criptografados');
+
+    let campoFoiModificado = false;
+
+    for (let i = 0; i < listaInputsTextoDescriptografados.length; i++) {
+
+        if (listaInputsTextoDescriptografados[i].value != descriptografados[i] ||
+            listaInputsTextoDescriptografados.length != descriptografados.length) {
+            campoFoiModificado = true;
+            break;
+        };
+    };
+
+    for (let i = 0; i < listaInputsCriptografados.length; i++) {
+
+        if (listaInputsCriptografados[i].value != criptografados[i] ||
+            listaInputsCriptografados.length != criptografados.length) {
+            campoFoiModificado = true;
+            break;
+        };
+    };
+    return campoFoiModificado;
+};
+
+function verificaSeAlgumCampoFoiRemovido(listaDeInputsTexto) {
+
+    let foiExcluido = false;
+
+    if (listaDeInputsTexto.length < (descriptografados.length + criptografados.length)) {
+        foiExcluido = true;
+    };
+    return foiExcluido;
+};
+
+function removeDoArrayOsValoresDosInputsExcluidos(indicesInputsRemovidos, arrayPrincipal) {
+
+    for (let i = 0; i < indicesInputsRemovidos.length; i++) {
+        arrayPrincipal.splice(indicesInputsRemovidos[i]);
+    };
+};
+
+function salvar() {
+
+    const listaDeInputsTexto = form.querySelectorAll('.criptografados, .descriptografados');
+
+    if (verificaSeAlgumCampoFoiRemovido(listaDeInputsTexto)) {
+        
+        removeDoArrayOsValoresDosInputsExcluidos(indicesDosInputsRemovidosDescriptografados, descriptografados);
+        removeDoArrayOsValoresDosInputsExcluidos(indicesDosInputsRemovidosCriptografados, criptografados);
+    };
+
+    atualizaOsValoresNoArray(listaDeInputsTexto);
+
+    alert("Salvo!");
+};
+
+function atualizaOsValoresNoArray(listaDeInputsTexto) {
+
+    let indiceCriptografados = 0;
+    let indiceDescriptografados = 0;
+
+    for (let i = 0; i < listaDeInputsTexto.length; i++) {
+
+        if (i % 2 === 0) {
+            descriptografados[indiceDescriptografados] = listaDeInputsTexto[i].value;
+            indiceDescriptografados++;
+        } else {
+            criptografados[indiceCriptografados] = listaDeInputsTexto[i].value;
+            indiceCriptografados++;
+        };
+    };
+};
+
+function removeElementoClicado(elemento, event) {
+
+
+    if (elemento.className == 'btn-excluir' && mainContainerInputs.children.length > 1) {
+
+        event.preventDefault();
+
+        const inputDescriptografados = elemento.parentElement.parentElement.querySelector('.descriptografados');
+        const inputCriptografados = elemento.parentElement.parentElement.querySelector('.criptografados');
+        const listaInputsTextoDescriptografados = form.querySelectorAll('.descriptografados');
+        const listaInputsTextoCriptografados = form.querySelectorAll('.criptografados');
+
+        const arrayDeInputsDescriptografados = converteNodeListEmUmArray(listaInputsTextoDescriptografados);
+        const arrayDeInputsCriptografados = converteNodeListEmUmArray(listaInputsTextoCriptografados);
+
+        const indiceDoInputDescriptografados = arrayDeInputsDescriptografados.indexOf(inputDescriptografados);
+        const indiceDoInputCriptografados = arrayDeInputsCriptografados.indexOf(inputCriptografados);
+
+        indicesDosInputsRemovidosDescriptografados.push(indiceDoInputDescriptografados);
+        indicesDosInputsRemovidosCriptografados.push(indiceDoInputCriptografados);
+
+        elemento.parentElement.parentElement.remove();
+        mostraOuEscondeBotaoAdicionar();
+
+    } else if (elemento.className == 'btn-excluir' && mainContainerInputs.children.length <= 1) {
+        event.preventDefault();
+    };
+};
+
+function converteNodeListEmUmArray(nodeList) {
+
+    const arrayNodeList = [];
+
+    for (let i = 0; i < nodeList.length; i++) {
+
+        let nodeItem = nodeList[i];
+        arrayNodeList.push(nodeItem);
+    };
+    return arrayNodeList;
+};
+
+function fechaEdit() {
+
+    const containerInputList = mainContainerInputs.querySelectorAll('.container-inputs');
+    let msg = false;
+
+    if (verificaSeAlgumCampoFoiModificado()) {
+
+        msg = confirm('Descartar alterações?');
+    };
+
+    if (msg || !verificaSeAlgumCampoFoiModificado()) {
+
+        for (let i = 0; i < containerInputList.length; i++) {
+            containerInputList[i].remove();
+        };
+
+        sectionPrincipal.removeChild(edit);
+        sectionPrincipal.classList.remove('editando');
+    };
+};
+
+function mostraOuEscondeBotaoAdicionar() {
+
+    if(form.querySelectorAll('.descriptografados, .criptografados').length < 10) {
+        btnAdicionar.style.display = 'inline-block';
+        btnAdicionar.style.marginRight = '16px';
+    } else {
+        btnAdicionar.style.display = 'none';
+    }
+};
+
+btnEditar.onclick = function (event) {
+
+    sectionPrincipal.classList.add('editando');
+
+    if(sectionPrincipal.contains(edit)) {
+        fechaEdit();
+        return;
+    } else {
+        mostraEditar();
+        event.preventDefault();
+    }
+    mostraOuEscondeBotaoAdicionar();
+};
+
+form.onclick = function (event) {
+
+    const elemento = event.target;
+
+    removeElementoClicado(elemento, event);
+};
+
+btnAdicionar.onclick = function(event) {
+    event.preventDefault();
+
+    if(mainContainerInputs.children.length <= 4) {
+        criaElementosDoForm();
+        mostraOuEscondeBotaoAdicionar();
+    };
+};
+
+btnSalvar.onclick = function (event) {
+
+    if (form.checkValidity()) {
+        event.preventDefault();
+        salvar();
+    };
+
+};
+
+btnFechaEdit.onclick = fechaEdit;
---
+++ script.js
@@ -0,0 +1,218 @@
+const texto = document.querySelector('.texto');
+const resultado = document.querySelector('.msg');
+const btnLimpar = document.querySelector('.btn-limpar');
+const btnCopiar = document.querySelector('.btn-copiar');
+const btnCriptografar = document.querySelector('.btn-criptografar');
+const btnDescriptografar = document.querySelector('.btn-descriptografar');
+
+const descriptografados = ['e', 'i', 'a', 'o', 'u'];
+const criptografados = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+
+btnCriptografar.onclick = function (event) {
+    event.preventDefault();
+
+    criptografaEDescriptografaTexto(descriptografados, criptografados);
+};
+
+btnDescriptografar.onclick = function (event) {
+    event.preventDefault();
+
+    criptografaEDescriptografaTexto(criptografados, descriptografados);
+};
+
+function criptografaEDescriptografaTexto(caracteresRemovidos, caracteresAdicionados) {
+
+    const textoModificado = [];
+    let textoDigitado = texto.value;
+    let posicao = 0;
+    let indiceCaractere = 0;
+    let caractereAtual = '';
+    let encontrouCaracteres = false;
+
+    if (!verificaSeTextoEValido(textoDigitado)) return;
+
+    while (posicao < textoDigitado.length) {
+
+        caractereAtual = caractereAtual + textoDigitado[posicao];
+
+        for (let i = 0; i < caracteresRemovidos.length; i++) {
+
+            if (caractereAtual == caracteresRemovidos[i]) {
+                textoModificado.push(caracteresAdicionados[i]);
+                caractereAtual = '';
+                indiceCaractere = 0;
+                break;
+
+            } else if (caractereAtual[0] == caracteresRemovidos[i][0]
+                && caractereAtual[indiceCaractere] == caracteresRemovidos[i][indiceCaractere]
+                && posicao < textoDigitado.length - 1) {
+
+                indiceCaractere++;
+                encontrouCaracteres = true;
+                break;
+
+            } else {
+                encontrouCaracteres = false;
+            }
+        };
+        if (!encontrouCaracteres) {
+            textoModificado.push(caractereAtual);
+            caractereAtual = '';
+            indiceCaractere = 0;
+        };
+        posicao++;
+    };
+
+    mostraResultado(textoModificado);
+};
+
+function verificaSeTextoEValido(texto) {
+
+    let valido = false;
+
+    if (texto.trimStart() != '') {
+
+        if (estaEmMinusculoESemAcento(texto)) {
+            document.querySelector('form span').style.color = '#000000';
+            valido = true;
+        } else {
+            document.querySelector('form span').style.color = '#ff0000';
+        }
+    } else {
+        resultado.value = '';
+        resultado.style.display = 'none';
+        resultado.previousElementSibling.style.display = 'block';
+        resultado.parentElement.style.justifyContent = 'center';
+        btnCopiar.style.display = 'none';
+    };
+    return valido;
+};
+
+function estaEmMinusculoESemAcento(texto) {
+
+    let textoValido = false;
+
+    for (let i = 0; i < texto.length; i++) {
+
+        let code = texto.charCodeAt(i);
+
+        if (code >= 97 && code <= 122 || code == 32) {
+            textoValido = true;
+        } else {
+            textoValido = false;
+            break;
+        };
+    };
+    return textoValido;
+};
+
+function mostraResultado(texto) {
+
+    resultado.style.display = 'inline-block';
+    resultado.previousElementSibling.style.display = 'none';
+    resultado.parentElement.style.justifyContent = 'space-between';
+    btnCopiar.style.display = 'inline-block';
+
+    resultado.value = texto.join('');
+    resultado.style.height = resultado.scrollHeight.toString() + 'px';
+    window.scrollTo({ top: 500, behavior: 'smooth' });
+};
+
+btnCopiar.onclick = function() {
+
+    navigator.clipboard.writeText(resultado.value);
+    this.innerHTML = 'Copiado!';
+
+};
+
+btnCopiar.onmouseout = function() {
+
+    this.innerHTML = 'Copiar';
+};
+
+btnLimpar.onclick = function (event) {
+
+    event.preventDefault();
+    texto.value = '';
+    texto.style.caretColor = 'transparent';
+};
+
+let interval;
+texto.onfocus = function() {
+
+    if (!this.value) {
+        interval = setInterval(placeholder, 500);
+    };
+};
+
+texto.onblur = function() {
+
+    this.placeholder = 'Digitar texto_';
+    clearInterval(interval);
+};
+
+texto.oninput = function() {
+
+    clearInterval(interval);
+
+    this.style.caretColor = '#0A3871';
+    if (!this.value) {
+        interval = setInterval(placeholder, 500);
+        this.style.caretColor = 'transparent';
+    };
+};
+
+let sublinhado = '_';
+function placeholder() {
+
+    texto.placeholder = 'Digitar texto' + sublinhado;
+
+    if (!sublinhado) {
+        sublinhado = '_';
+    } else {
+        sublinhado = '';
+    };
+};
+
+document.onclick = function(event) {
+
+    const elemento = event.target;
+
+    if(elemento.className == 'top') {
+        window.scrollTo(0, 0);
+    };
+};
+
+let btnScrollEstaVisivel = false;
+
+window.onscroll = rolarAPaginaParaTopo;
+
+function rolarAPaginaParaTopo() {
+
+    const btnScroll = document.querySelector('.top');
+
+    if(window.scrollY == 0) {
+        if(btnScroll != null) {
+            btnScroll.style.display = 'none';
+        };
+
+    } else {
+        if(btnScroll != null) {
+            btnScroll.style.display = 'inline-block';
+        };
+    };
+    if(btnScrollEstaVisivel) {
+        return;
+    } else {
+        botaoDeRolagemParaOTopo();
+        btnScrollEstaVisivel = true;
+    };
+};
+function botaoDeRolagemParaOTopo() {
+
+    const btnScroll = document.createElement('button');
+
+    btnScroll.classList.add('top');
+    document.body.appendChild(btnScroll);
+};
+
